eeafad06b6b9ac3416c555a3e50fe6022c6b0484
shimjim7 <kamya030btit21@igdtuw.ac.in>
2023-06-05 13:09:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from azure.storage.blob import BlobServiceClient
 
 load_dotenv()
 
-
 def get_weather_forecast(api_key, city):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 
@@ -48,7 +47,8 @@ def get_weather_forecast(api_key, city):
 
         # Perform historical weather data analysis
         historical_data = get_historical_weather_data(city)
-        analyze_historical_data(historical_data)
+        if historical_data is not None:
+            analyze_historical_data(historical_data)
     else:
         print("Unable to fetch weather data. Please try again.")
 
@@ -63,7 +63,6 @@ def perform_sentiment_analysis(text):
     sentiment_score = response[0].confidence_scores
     print(f"Sentiment scores: Positive={sentiment_score.positive:.2f}, Negative={sentiment_score.negative:.2f}, Neutral={sentiment_score.neutral:.2f}")
 
-
 def retrieve_location_coordinates(location):
     geolocator = Nominatim(user_agent="weather-forecast")
     location_data = geolocator.geocode(location)
@@ -72,9 +71,11 @@ def retrieve_location_coordinates(location):
 
 def display_weather_on_map(coordinates, weather):
     maps_subscription_key = os.getenv("AZURE_MAPS_SUBSCRIPTION_KEY")
+    latitude, longitude = coordinates
 
-    map_url = f"https://atlas.microsoft.com/map/static/png?subscription-key={maps_subscription_key}&api-version=1.0&center={coordinates[0]},{coordinates[1]}&zoom=12&layer=basic&height=500&width=800"
+    map_url = f"https://atlas.microsoft.com/map/static/png?subscription-key={maps_subscription_key}&api-version=1.0&center={longitude},{latitude}&zoom=10&layer=basic&height=500&width=800"
     print("Weather forecast map:")
+    print(f"Weather: {weather}")
     print(map_url)
 
 def store_weather_data(city, data):
@@ -91,33 +92,61 @@ def store_weather_data(city, data):
     blob_client.upload_blob(data_bytes, overwrite=True)
 
 def get_historical_weather_data(city):
-    # Retrieve historical weather data
-    historical_data = {}  # Placeholder for historical weather data retrieval logic
-    return historical_data
+    api_key = os.getenv("OPENWEATHERMAP_API_KEY")
+    base_url = "https://api.openweathermap.org/data/2.5/forecast"
+    params = {
+        "q": city,
+        "appid": api_key,
+        "units": "metric"
+    }
+
+    response = requests.get(base_url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        daily_forecasts = data["list"]
+
+        historical_data = []
+        for forecast in daily_forecasts:
+            date = pd.to_datetime(forecast['dt'], unit='s').date()
+            temperature = forecast['main']['temp']
+            humidity = forecast['main']['humidity']
+            weather_description = forecast['weather'][0]['description']
+
+            historical_data.append({
+                'date': date,
+                'temperature': temperature,
+                'humidity': humidity,
+                'weather': weather_description
+            })
+
+        return historical_data
+    else:
+        print("Unable to fetch historical weather data. Please try again.")
+        return None
+
 
 def analyze_historical_data(historical_data):
-    # Process and analyze historical weather data
-    # Example code for analysis and visualization
-    df = pd.DataFrame.from_dict(historical_data)
-    df['date'] = pd.to_datetime(df['date'])
-    df.set_index('date', inplace=True)
+    if len(historical_data) > 0:
+        df = pd.DataFrame.from_records(historical_data)
+        df['date'] = pd.to_datetime(df['date'])
+        df.set_index('date', inplace=True)
 
-    # Calculate statistics
-    mean_temperature = df['temperature'].mean()
-    max_temperature = df['temperature'].max()
-    min_temperature = df['temperature'].min()
+        mean_temperature = df['temperature'].mean()
+        max_temperature = df['temperature'].max()
+        min_temperature = df['temperature'].min()
 
-    # Plot temperature over time
-    plt.plot(df.index, df['temperature'])
-    plt.title('Historical Temperature')
-    plt.xlabel('Date')
-    plt.ylabel('Temperature (°C)')
-    plt.show()
+        plt.plot(df.index, df['temperature'])
+        plt.title('Historical Temperature')
+        plt.xlabel('Date')
+        plt.ylabel('Temperature (°C)')
+        plt.show()
 
-    print("Historical Weather Data Analysis:")
-    print(f"Mean Temperature: {mean_temperature} °C")
-    print(f"Max Temperature: {max_temperature} °C")
-    print(f"Min Temperature: {min_temperature} °C")
+        print("Historical Weather Data Analysis:")
+        print(f"Mean Temperature: {mean_temperature:.2f} °C")
+        print(f"Max Temperature: {max_temperature:.2f} °C")
+        print(f"Min Temperature: {min_temperature:.2f} °C")
+    else:
+        print("No historical weather data available.")
 
 if __name__ == '__main__':
     api_key = os.getenv("OPENWEATHERMAP_API_KEY")
